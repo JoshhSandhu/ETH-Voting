7f123cf4d99a5f0a6bde71562b45018d410d2d48
hema-siva <hema@progaindia.com>
2025-03-08 17:37:20 +0530

    added a few comments

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -4,17 +4,17 @@ contract Election{
 
     struct Candidate { //structure of the candidate
         uint id;
-        string name;
+        string name; //state variables
         uint voteCount;
     }
 
-    mapping(uint => Candidate) public candidates;
+    mapping(uint => Candidate) public candidates; //this is like a hashmap or a dict where unit is the key and candicate is the value
 
     uint public candidatesCount; //too keep track of how many candidates there are
 
     constructor () public{
         addCandidate("Candidate 1");
-        addCandidate("Candidate 2");
+        addCandidate("Candidate 2"); //these are constructors they will be deleted after the contract has been created
     }
 
     function addCandidate (string memory _name) private {
